a7e4eff2cb021be5b5b070cbb2fdc31eec22a157
Administrator <Administrator@TR-IB-02095>
2016-04-22 19:07:11 +0800

    更新

--- src/zf/test/FpsTest.java
+++ src/zf/test/FpsTest.java
@@ -1,8 +1,13 @@
 package zf.test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,8 +19,9 @@ public class FpsTest {
 	 * 使用脚本前请先在手机开发者模式中开启GPU呈现模式
 	 * @param args
 	 */
-	public static String[] getCmd(String CMD) {
+	public static int getCmd(String CMD) {
 		String[] output = null;
+		int fps2 = 0;
 		boolean aa = false;
 		try {
 			Process p = Runtime.getRuntime().exec(CMD);
@@ -42,8 +48,10 @@ public class FpsTest {
 
 			b = a.substring(a.indexOf("Execute"),a.indexOf("/n/n/n/n"));
             String[] b1 = b.split("/n/n");
+//            System.out.println(Arrays.toString(b1));
             float sum = 0;
             int framesDropped = 0;
+            int vsync_overtime = 0;
 			for(int i1=1;i1<b1.length;i1++) {				
 				String c = b1[i1].trim();
 				String[] d = c.split("	");
@@ -52,31 +60,40 @@ public class FpsTest {
 					b2 +=  Float.parseFloat(d[ii].trim());
 				}
 				//时间小于16ms的视为16ms
-				if(b2 < 16) {
+				if(b2 < 16.67) {
 					b2 = (float) 16.67;
 				}
 				sum += b2;
-				if(b2 > 16.7) {
-					System.out.println(b2);
+				if(b2 > 16.67) {
+					if(b2%16.67 == 0) {
+						vsync_overtime = (int) (b2%16.67 - 1)+vsync_overtime;
+					}else {
+						vsync_overtime = (int) (b2%16.67)+vsync_overtime;
+					}
+//					System.out.println(b2);
 					framesDropped++;
 				}
 			}	
+			
 			int sumTime = b1.length-1;
+//			System.out.println("掉帧数："+vsync_overtime);
+			fps2 = sumTime * 60 / (sumTime + vsync_overtime);
 			float A = sum/sumTime;
 			float pctFramesDropped = (float)framesDropped/sumTime*100;
 			String pct = new DecimalFormat("0.00").format(pctFramesDropped); 
 			String AVG = new DecimalFormat("0.00").format(A);
 			String fps = new DecimalFormat("0.00").format(1000/A);
-			System.out.println("总帧数："+sumTime);
-			System.out.println("每帧平均完成时间："+AVG+"ms");
-			System.out.println("平均帧率："+fps+"fps");
-			System.out.println("掉帧数："+framesDropped);
-			System.out.println("掉帧率："+pct+"%");
+//			System.out.println("总帧数："+sumTime);
+//			System.out.println("每帧平均完成时间："+AVG+"ms");
+//			System.out.println("旧帧率："+fps+"fps");
+			System.out.println("帧率："+fps2+"fps");
+//			System.out.println("掉帧数："+framesDropped);
+//			System.out.println("掉帧率："+pct+"%");
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return output;
+		return fps2;
 	}
 	
 	public static String getPackage() {
@@ -98,21 +115,54 @@ public class FpsTest {
 		return sub;
 	}
 	
-	public static String getFtp(String Package) {
+	public static int getFtp(String Package) {
 		String CMD = "adb shell dumpsys gfxinfo "+Package;
-		String[] out = getCmd(CMD);
-		return null;
+		int out = getCmd(CMD);
+		return out;
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String Package = "";
+		FileWriter fw = null;
+		File f = new File("d:/fps.csv");
+		fw = new FileWriter("d:/fps.csv");
+		if(!f.exists()) {
+			f.createNewFile();
+		}else {
+			f.delete();
+			f.createNewFile();
+		}
+		
 		if(!Package.equals(getPackage())) {
 			Package = getPackage();
 			System.out.println("当前应用包名："+getPackage());
 		}
-		getFtp(getPackage());
-
+		for(int i=0; i<300; i++) {
+			try {
+//				System.out.println(getFtp(getPackage()));
+				int fps = getFtp(getPackage());
+				fw.write(String.valueOf(fps)+"\n");
+				fw.flush(); 
+				if(fps<60) {
+					Runtime.getRuntime().exec("cmd /c adb shell screencap -p /sdcard/"+i+".png");
+				}
+			}catch(Exception e) {
+//				e.printStackTrace();
+				try {
+					fw.write("0"+"\n");
+					fw.flush();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				System.out.println("帧率：0fps");
+			}
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		fw.close();
 	}
-
 }
