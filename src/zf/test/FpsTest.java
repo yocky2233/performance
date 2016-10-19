eb0e9c1c3981d092ba52da3022064d8e7f1e155d
郑仲富 <@DESKTOP-D1HPJE8>
2016-10-19 20:21:20 +0800

    更新

--- src/zf/test/FpsTest.java
+++ src/zf/test/FpsTest.java
@@ -24,6 +24,8 @@ public class FpsTest {
 	 * @param args
 	 */
 	public static int getCmd(String CMD) {
+		FpsTest ft = new FpsTest();
+		
 		String[] output = null;
 		int fps2 = 0;
 		boolean aa = false;
@@ -42,7 +44,7 @@ public class FpsTest {
 //						a.delete(0, a.length());
 //					}
 					
-					System.out.println("包含Process	Execute");
+//					System.out.println("包含Process	Execute");
 					aa = true;
 					number += 1;
 				}
@@ -54,18 +56,27 @@ public class FpsTest {
 				}
 			}
 			
-			System.out.println(a);
+//			System.out.println("抓取出来的："+a);
 			
 			Pattern pt = Pattern.compile("\\d+\\.\\d+\\s+\\d+\\.\\d+.*\\d+\\.\\d+/n/n");
 	        Matcher m = pt.matcher(a);//正则过滤
 	        while(m.find()) {    
-	            System.out.println("过滤的值："+m.group());  
+//	            System.out.println("过滤的值："+m.group());  
 	            b = m.group();
 	        }
 
 //			b = a.substring(a.indexOf("Execute"),a.indexOf("/n/n/n/n"));
-            String[] b1 = b.split("/n/n");
-            System.out.println(Arrays.toString(b1));
+	        String[] b1 = null;
+	        try{
+	        	b1 = b.split("/n/n");
+	        }catch(Exception e) {
+	        	System.out.println("请开启GPU呈现模式");
+	        	ft.killMonkey();
+				System.exit(0);
+	        }
+            
+            
+//            System.out.println(Arrays.toString(b1));
             float sum = 0;
             int framesDropped = 0;
             int vsync_overtime = 0;
@@ -73,7 +84,7 @@ public class FpsTest {
 //				System.out.println(b1[i1]);
 				String c = b1[i1].trim();
 				String[] d = c.split("	");
-				System.out.println(Arrays.toString(d));
+//				System.out.println(Arrays.toString(d));
 				double b2 = 0;
 				
 				if(d.length >= 3) {
@@ -82,7 +93,7 @@ public class FpsTest {
 					}
 				}
 				
-				System.out.println("帧率时间："+b2);
+//				System.out.println("帧率时间："+b2);
 				
 				//时间小于16ms的视为16ms
 				if(b2 < 16.67) {
@@ -90,7 +101,10 @@ public class FpsTest {
 				}
 				sum += b2;
 				if(b2 > 16.67) {
-					System.out.println("掉帧时间："+b2);
+					if(b2 > 33.34) {
+						System.out.println("第"+i1+"帧掉帧时间："+b2+" 大于3帧时间");
+					}
+					
 					if(b2%16.67 == 0) {
 						vsync_overtime = (int) (b2/16.67 - 1)+vsync_overtime;
 					}else {
@@ -139,8 +153,9 @@ public class FpsTest {
 		}
 		
 		String at = out.substring(out.indexOf("Y")+1,out.indexOf("pid")).trim();
-		activity = at.split(" ")[0].trim();
-		System.out.println(activity);
+		String at2 = at.split(" ")[0].trim();
+		activity = at2.split("/")[1].trim();
+//		System.out.println("activity是："+activity);
 		String sub = out.substring(out.indexOf("Y")+1,out.indexOf("/"));
 		return sub;
 	}
@@ -152,7 +167,7 @@ public class FpsTest {
 	}
 	
 	
-	public void test() throws IOException {
+	public void test(int times) throws IOException {
 //		String Package = "com.android.systemui";
 		String Package = "";
 		FileWriter fw = null;
@@ -171,13 +186,17 @@ public class FpsTest {
 		
 		if(!Package.equals(getPackage())) {
 			Package = getPackage();
-			System.out.println("当前应用包名："+getPackage());
+			System.out.println("当前应用包名："+Package);
 		}
-		for(int i=0; i<1; i++) {
+		
+		Runtime.getRuntime().exec("adb shell dumpsys gfxinfo "+Package);//先清除一下数据
+		for(int i=1; i<times; i++) {
+			System.out.println("第"+i+"次取值");
 			try {
-				int fps = getFtp(Package);
+				int fps = getFtp(Package); //获取帧率
 				fw.write(String.valueOf(fps)+"\n");
 				fw.flush(); 
+				Thread.sleep(2000);
 			}catch(Exception e) {
 				e.printStackTrace();
 				try {
@@ -188,24 +207,186 @@ public class FpsTest {
 				}
 				System.out.println("帧率：0fps");
 			}
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
 		}
+		
 		fw.close();
 	}
 	
 	
-	public static void main(String[] args)  {
-		FpsTest t = new FpsTest();
+	public void create(String direction) {
+		String []xy = getXY();
+		int x = Integer.parseInt(xy[0]);
+		int y = Integer.parseInt(xy[1]);
+		String startX = null;
+		String startY = null;
+		String stopX = null;
+		String stopY = null;
+		
+		String BS;
+		
+		
+		
+		File moneky = new File("d:/monekyFile");
+		if(!moneky.exists()) {
+			try {
+				moneky.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(direction.equals("horizontal")) {
+			
+			double HY = 0.5;
+			double startHX = 0.8;
+			double stopHX = 0.2;
+			
+			startY = stopY = new DecimalFormat("0").format(HY*y);
+			startX = new DecimalFormat("0").format(startHX*x);
+			stopX = new DecimalFormat("0").format(stopHX*x);
+			
+			double BSH = Math.abs(startHX*x - stopHX*x)/8;
+			BS = new DecimalFormat("0").format(BSH);
+		}else {
+			double VX = 0.5;
+			double startVY = 0.7;
+			double stopVY = 0.3;
+			
+			startX = stopX = new DecimalFormat("0").format(VX*x);
+			startY = new DecimalFormat("0").format(startVY*y);
+			stopY = new DecimalFormat("0").format(stopVY*y);
+			
+			double BSV = Math.abs(startVY*y - stopVY*y)/8;
+			BS = new DecimalFormat("0").format(BSV);
+		}
+		
 		try {
-			t.test();
+			
+			FileWriter fw = new FileWriter("d:/monekyFile");
+			fw.write("type = user");
+			fw.write("\n");
+			fw.write("count = 10");
+			fw.write("\n");
+			fw.write("speed = 1.0");
+			fw.write("\n");
+			fw.write("start data >>");
+			fw.write("\n");
+			fw.write("UserWait(600)");
+			fw.write("\n");
+			fw.write("Drag("+startX+","+startY+","+stopX+","+stopY+","+BS+")");
+			fw.write("\n");
+			fw.write("UserWait(600)");
+			fw.write("\n");
+			fw.write("Drag("+stopX+","+stopY+","+startX+","+startY+","+BS+")");
+			fw.flush();
+			fw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		getPackage();
+	}
+	
+	public String[] getXY() {
+		String[] XY = new String[2];
+		try {
+			Process p = Runtime.getRuntime().exec("adb shell wm size");
+			
+			BufferedReader read = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+			String a;
+			while((a=read.readLine())!=null) {
+				if(a.contains("Physical size")) {
+					String resolution = a.split(":")[1].trim();
+					XY[0] = resolution.split("x")[0].trim();
+					XY[1] = resolution.split("x")[1].trim();
+				}
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Arrays.toString(XY));
+		return XY;
 		
+	}
+	
+	// 获取窗口命令输出
+		public StringBuffer cmd(String cmd, String split) {
+			Process p = null;
+			StringBuffer sb = null;
+			try {
+				p = Runtime.getRuntime().exec(cmd);
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						p.getInputStream()));
+				sb = new StringBuffer();
+				String a = null;
+				while ((a = br.readLine()) != null) {
+					sb.append(a + split);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return sb;
+		}
+	
+	// 获取monkey的pid
+		public void killMonkey() {
+			String pid = null;
+			Process p = null;
+			StringBuffer sb = null;
+			
+			String sbPid = cmd("adb  shell ps | grep monkey"," ").toString().trim();
+			
+			Pattern p1 = Pattern.compile("[0-9]+");
+			Matcher m = p1.matcher(sbPid);
+			while (m.find()) {
+				String id = m.group();
+				if (id.length() > 2) {
+					pid = id;
+					break;
+				}
+			}
+			
+			try {
+				Runtime.getRuntime().exec("adb shell kill "+ pid);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	
+	public static void main(String[] args) throws InterruptedException  {
+		FpsTest t = new FpsTest();
+		t.create("h");
+		
+		try {
+			Process p = Runtime.getRuntime().exec("adb push d:/monekyFile /mnt/sdcard/");
+            BufferedReader read = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+			String a;
+			while((a=read.readLine())!=null) {
+				System.out.println(a);
+			}
+			
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		Process run = null;
+		try {
+			run = Runtime.getRuntime().exec("adb shell monkey -v -f /mnt/sdcard/monekyFile 50");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+		Thread.sleep(3000);
+		
+		try {
+			t.test(11);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		t.killMonkey();
+		System.out.println("测试结束……");
 	}
 }
